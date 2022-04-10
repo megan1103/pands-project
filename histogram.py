f9de1ec808840cf2c5e2b09f56940189b7c77c95
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-10 19:17:36 +0100

    Histogram
    Created histograms for each variable

---
+++ histogram.py
@@ -0,0 +1,52 @@
+# Programming and Scripting
+# Project 2022
+# Author: Megan O'Donovan
+# Summary: Create a histogram of each variable
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as pl
+import seaborn as sns
+
+
+# Load the dataset into python
+csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
+iris =  pd.read_csv(csv_file)
+#print(iris.head(10)) 
+
+pl.subplot(2, 2, 1)  ### want to create a 4x4 view https://nickmccullum.com/python-visualization/subplots/
+# First Plot  
+# Plot histogram of  petal lengths
+_ = pl.hist(iris.petal_length)
+
+# Label axes
+_ = pl.xlabel('petal length (cm)')
+_ = pl.ylabel('count')
+
+#Second Plot
+# Plot histogram of petal width
+pl.subplot(2, 2, 2)    
+_ = pl.hist(iris.petal_width)
+
+# Label axes
+_ = pl.xlabel('petal width (cm)')
+_ = pl.ylabel('count') 
+
+#Third Plot
+pl.subplot(2, 2, 3)    
+# Plot histogram of sepal lengths
+_ = pl.hist(iris.sepal_length)
+
+# Label axes
+_ = pl.xlabel('sepal length (cm)')
+_ = pl.ylabel('count')
+
+#Fourth Plot
+# Plot histogram of sepal width
+pl.subplot(2,2,4)
+_ = pl.hist(iris.sepal_width)
+
+# Label axes
+_ = pl.xlabel('sepal width (cm)')
+_ = pl.ylabel('count') 
+# Show histogram
+pl.show()
