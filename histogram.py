34054cbdd5f59238713d3a908e11a2323987a332
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-12 19:29:33 +0100

    histogram.py
    Found an easier method of plotting all four histograms into one view

--- histogram.py
+++ histogram.py
@@ -11,42 +11,51 @@ import seaborn as sns
 # Load the dataset into python
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
 iris =  pd.read_csv(csv_file)
-#print(iris.head(10)) 
+##print(iris.head(10)) 
 
-pl.subplot(2, 2, 1)  ### want to create a 4x4 view https://nickmccullum.com/python-visualization/subplots/
-# First Plot  
-# Plot histogram of  petal lengths
-_ = pl.hist(iris.petal_length)
+#pl.subplot(2, 2, 1)  ### want to create a 4x4 view https://nickmccullum.com/python-visualization/subplots/
+## First Plot  
+## Plot histogram of  petal lengths
+#_ = pl.hist(iris.petal_length)
 
-# Label axes
-_ = pl.xlabel('petal length (cm)')
-_ = pl.ylabel('count')
+## Label axes
+#_ = pl.xlabel('petal length (cm)')
+#_ = pl.ylabel('count')
 
-#Second Plot
-# Plot histogram of petal width
-pl.subplot(2, 2, 2)    
-_ = pl.hist(iris.petal_width)
+##Second Plot
+## Plot histogram of petal width
+#pl.subplot(2, 2, 2)    
+#_ = pl.hist(iris.petal_width)
 
-# Label axes
-_ = pl.xlabel('petal width (cm)')
-_ = pl.ylabel('count') 
+## Label axes
+#_ = pl.xlabel('petal width (cm)')
+#_ = pl.ylabel('count') 
 
-#Third Plot
-pl.subplot(2, 2, 3)    
-# Plot histogram of sepal lengths
-_ = pl.hist(iris.sepal_length)
+##Third Plot
+#pl.subplot(2, 2, 3)    
+## Plot histogram of sepal lengths
+#_ = pl.hist(iris.sepal_length)
 
-# Label axes
-_ = pl.xlabel('sepal length (cm)')
-_ = pl.ylabel('count')
+## Label axes
+#_ = pl.xlabel('sepal length (cm)')
+#_ = pl.ylabel('count')
 
-#Fourth Plot
-# Plot histogram of sepal width
-pl.subplot(2,2,4)
-_ = pl.hist(iris.sepal_width)
+##Fourth Plot
+## Plot histogram of sepal width
+#pl.subplot(2,2,4)
+#_ = pl.hist(iris.sepal_width)
 
-# Label axes
-_ = pl.xlabel('sepal width (cm)')
-_ = pl.ylabel('count') 
-# Show histogram
-pl.show()
+## Label axes
+#_ = pl.xlabel('sepal width (cm)')
+#_ = pl.ylabel('count') 
+## Show histogram
+#pl.show()
+
+dataset = pd.DataFrame(iris)
+#df = dataset.rename  = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
+df = dataset.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.gca()
+df.hist(ax=ax)
+plt.show()
