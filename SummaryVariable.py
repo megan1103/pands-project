edf7f4619e92697be57275a71598e68266d61cdc
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-25 18:05:55 +0100

    Update SummaryVariable.py
    Cleaned up the code

--- SummaryVariable.py
+++ SummaryVariable.py
@@ -1,18 +1,26 @@
 
-import pandas as pd  
-# 2. Outputs a summary of each variable to a single text file
-# Create a variable `csv_file` and pass to it the csv where the data set is available
-import pandas as pd  
-# Load the dataset into python
+# Programming and Scripting
+# Project 2022
+# Author: Megan O'Donovan
+# Summary: Outputs a summary of each variable using decribe and info
 
+import pandas as pd  
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
-
 iris =  pd.read_csv(csv_file)
+iris = iris.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
 
-#print(df['sepal_length'].describe()) # Summary statistics of column Sepal_Length
+# Summary statistics of column Sepal_Length
+print(iris.describe())
 
-#print(df.info()) ##The output of the .info() method shows you the number of rows (or entries) and the number of columns, as well as the columns names and the types of data they contain (e.g. float64 which is the default decimal type in Python).
+# Summary statistics of column Sepal_Length
+print(iris['Sepal-Length'].describe())
 
-## group the example dataframe by the species and then run the describe() method on iris. This would run .describe() on the sepal_length values for each season as a grouped dataset. 
-precip_by_season=iris.groupby(["species"])[["sepal_length"]].describe()
-print(precip_by_season)
+#The output of the .info() method shows you the number of rows (or entries) and the number of columns, 
+#as well as the columns names and the types of data they contain (e.g. float64 which is the default decimal type in Python).
+print(iris.info())
+
+## Groups the example dataframe by the species and then run the describe() method on iris. 
+#This would run .describe() on the sepal_length values for each season as a grouped dataset. 
+
+describe_species=iris.groupby(["Species"])[["Sepal-Length"]].describe()
+print(describe_species)
