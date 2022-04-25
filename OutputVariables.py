463772116192b9dbd7fe20fa453d3fa9cf5f025e
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-25 17:40:48 +0100

    Update OutputVariables.py
    Removed extra code

--- OutputVariables.py
+++ OutputVariables.py
@@ -1,17 +1,10 @@
+# Programming and Scripting
+# Project 2022
+# Author: Megan O'Donovan
+# Summary: Imports the csv file and then exports the csv file to local desktop
 
 import pandas as pd  
-# 2. Outputs a summary of each variable to a single text file
-# Create a variable `csv_file` and pass to it the csv where the data set is available
-import pandas as pd  
-# Load the dataset into python
-
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
-
 iris =  pd.read_csv(csv_file)
-
-#print(iris) # prints to data frame
-
-df = pd.DataFrame(iris)
-
-df.to_csv (r'C:/Users/odonovanm/Documents/iris datasetExport.csv', index = False, header=True)
+iris.to_csv (r'C:/Users/odonovanm/Documents/iris datasetExport1.csv', index = False, header=True)
 
