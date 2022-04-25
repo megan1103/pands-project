0a14cc3e34a868983f7d077a0ec83ac44247963b
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-25 18:12:45 +0100

    Update correlationPlot.py
    Cleaned up code

--- correlationPlot.py
+++ correlationPlot.py
@@ -1,3 +1,8 @@
+# Programming and Scripting
+# Project 2022
+# Author: Megan O'Donovan
+# Summary: Create a correlation table and a heat map to see if there is a strong relationship between features
+
 import numpy as np    
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,10 +10,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
 iris =  pd.read_csv(csv_file)
-dataset = pd.DataFrame(iris)
-#df = dataset.rename  = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
-df = dataset.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
+iris = iris.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
 
 # Plotting heat map
-sns.heatmap(df.corr(),  linecolor = 'white', linewidths = 1,annot=True) ## annot adds the numeric value of the tile
+sns.heatmap(iris.corr(),  linecolor = 'white', linewidths = 1,annot=True) ## annot adds the numeric value of the tile
 plt.show()
+
+# Plot a correlation table
+print(iris.corr())
+
