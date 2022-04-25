b88670cbd536477a0885b67f018e865bb2060dbb
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-25 17:37:17 +0100

    Update ChangingNames.py
    Removed redundant code

--- ChangingNames.py
+++ ChangingNames.py
@@ -1,13 +1,11 @@
 # Programming and Scripting
 # Project 2022
 # Author: Megan O'Donovan
-# Summary: Using describe that gives you the count, mean, max, min of the data in a tabular format.
-import numpy as np    
-import pandas as pd
-import matplotlib.pyplot as plt
+# Summary: Using rename to change the column names in the data file
+# pands library allows csv to be imported/read
+import pandas as pd 
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
 iris =  pd.read_csv(csv_file)
-dataset = pd.DataFrame(iris)
-#df = dataset.rename  = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
-df = dataset.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
+
+df = iris.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
 print(df)
