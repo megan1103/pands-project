6d6703e2566c829bb557cce194b5f1c6ffaac7e3
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-25 18:13:33 +0100

    Update correlationPlot.py

--- correlationPlot.py
+++ correlationPlot.py
@@ -3,11 +3,10 @@
 # Author: Megan O'Donovan
 # Summary: Create a correlation table and a heat map to see if there is a strong relationship between features
 
-import numpy as np    
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import seaborn as sns
+
 csv_file = 'C:/Users/odonovanm/Documents/iris dataset.csv'
 iris =  pd.read_csv(csv_file)
 iris = iris.rename({'sepal_length': 'Sepal-Length', 'sepal_width': 'Sepal-Width', 'petal_length': 'Petal-Length', 'petal_width': 'Petal-Width', 'species': 'Species'}, axis=1)
